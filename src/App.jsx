cb94e918d642eb2cb604089b9a985cf418bcfeb6
Faizan-Ahmad-Khan <fa3n20004@gmail.com>
2024-07-27 21:57:31 +0500

    react code to make student management system

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import CreatStudent from './components/CreatStudent';
+import StudentList from './components/StudentList';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './FirebaseConfig';
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [loader, setLoader] = useState(false);
+
+  const studentsCollection = collection(db, "students");
+
+  const getStudents = async () => {
+    setLoader(true); 
+    try {
+      const studentSnapShot = await getDocs(studentsCollection);
+      const studentList = studentSnapShot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setStudents(studentList);
+    } catch (error) {
+      console.log(error);
+    } 
+      setLoader(false); 
+
+  };
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  return (
+    <div className='app-container'>
+      <h1 className='app-title'>Student Management System</h1>
+      <CreatStudent getStudents={getStudents}  />
+      {loader ? <p>Loading...</p> : <StudentList students={students} setStudents={setStudents}  />}
+    </div>
+  );
+}
+
+export default App;
